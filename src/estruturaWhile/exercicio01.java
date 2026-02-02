92f26ac34d0d8d62f0b3dd9d5f4b4f79ba017fcb
phgazana <phgazana@gmail.com>
2026-02-02 07:53:24 -0300

    init commit

---
+++ src/estruturaWhile/exercicio01.java
@@ -0,0 +1,17 @@
+package estruturaWhile;
+
+import javax.swing.*;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+
+        int senha;
+        do {
+            senha = Integer.parseInt(JOptionPane.showInputDialog("Insira a senha:"));
+            if (senha != 2002) {
+                System.out.println("Senha Invalida");
+            }
+        } while (senha != 2002);
+        System.out.println("Acesso Permitido");
+    }
+}
---
+++ src/estruturaWhile/exercicio03.java
@@ -0,0 +1,27 @@
+package estruturaWhile;
+
+import javax.swing.*;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+
+        int codigo;
+        int alcool = 0, gasolina = 0, diesel = 0;
+        do {
+            codigo = Integer.parseInt(JOptionPane.showInputDialog("Insira o código do produto:"));
+            if (codigo == 1) {
+                alcool++;
+            }
+            if (codigo == 2) {
+                gasolina++;
+            }
+            if (codigo == 3) {
+                diesel++;
+            }
+        } while (codigo != 4);
+        System.out.println("MUITO OBRIGADO\n" +
+                "Alcool: " + alcool +
+                "\nGasolina: " + gasolina +
+                "\nDiesel: " + diesel);
+    }
+}
