5f8d7978d94903dc965f8e9206d578b97056c3c0
phgazana <phgazana@gmail.com>
2026-02-03 18:33:19 -0300

    init commit

---
+++ src/estruturaFor/exercicio04.java
@@ -0,0 +1,25 @@
+package estruturaFor;
+
+import javax.swing.*;
+import java.util.*;
+
+public class exercicio04 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Insira um número:"));
+
+        for (int i = 0; i < n; i++) {
+            int div1 = scanner.nextInt();
+            int div2 = scanner.nextInt();
+
+            if (div2 == 0) {
+                System.out.println("divisão impossivel");
+            } else {
+                double div = (double) div1 / div2;
+                System.out.printf("%.1f%n", div);
+            }
+        }
+    }
+}
---
+++ src/estruturaFor/exercicio06.java
@@ -0,0 +1,15 @@
+package estruturaFor;
+
+import javax.swing.*;
+
+public class exercicio06 {
+    public static void main(String[] args) {
+
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Insira um número:"));
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
